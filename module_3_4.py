c2e23b59a066aa42c690afcd378062fecfb4a580
@andrei_kotov1 <kotov.aa.logist@gmail.com>
2024-11-13 21:02:24 +0300

    task 4 (module 3) - corrected

--- module_3_4.py
+++ module_3_4.py
@@ -1,18 +1,9 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for word in other_words:
-        if is_intersection(root_word, word, 4):
+        if (root_word.upper() in word.upper()) or (word.upper() in root_word.upper()):
             same_words.append(word)
     return same_words
-
-def is_intersection(word1, word2, count):
-    result = False
-    for i in range(0, len(word1) - count + 1):
-        test_str = (word1[i: i + count]).upper()
-        if test_str in word2.upper():
-            result = True
-            break
-    return result
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
