bfc7793f3b04169c9c796256c231ae027d2fc47c
@andrei_kotov1 <kotov.aa.logist@gmail.com>
2024-11-19 21:30:15 +0300

    task Hard (module 3)

---
+++ module_3_hard.py
@@ -0,0 +1,29 @@
+def calculate_structure_sum(args):
+    res = 0
+    for arg in args:
+        if isinstance(arg, int):
+            res = res + arg
+        if isinstance(arg, str):
+            res = res + len(arg)
+        if isinstance(arg, list):
+            res = res + calculate_structure_sum(arg)
+        if isinstance(arg, tuple):
+            res = res + calculate_structure_sum(arg)
+        if isinstance(arg, set):
+            res = res + calculate_structure_sum(arg)
+        if isinstance(arg, dict):
+            res = res + calculate_structure_sum(arg.keys())
+            res = res + calculate_structure_sum(arg.values())
+    return res
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
