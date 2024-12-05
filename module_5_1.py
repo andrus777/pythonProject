bab2ba7a334b1356ca4f102bc9373b8f139f10fc
andrus777 <kotov.aa.logist@gmail.com>
2024-12-05 16:52:29 +0300

    Module 5 task 2 (OOP)

--- module_5_1.py
+++ module_5_1.py
@@ -10,7 +10,20 @@ class House:
         else:
             print('Такого этажа не существует')
 
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return "Название: " + str(self.name) + ", кол-во этажей: " + str(self.number_of_floors)
+
+
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(18)
 h2.go_to(3)
+
+print(h1)
+print(h2)
+
+print(len(h1))
+print(len(h2))
