66f3029b801ec41222a938b1ed5363a8eb2b1f2a
andrus777 <kotov.aa.logist@gmail.com>
2024-12-13 15:27:33 +0300

    Practic_user_reg 1-3

--- practic_user_reg.py
+++ practic_user_reg.py
@@ -15,7 +15,41 @@ class Database:
     def add_user(self, username, password):
         self.data[username] = password
 
+    def is_login(self, login):
+        if self.data.get(login, 0) == 0:
+            return False
+        else:
+            return True
+    def is_autorize(self, login, password):
+        test = self.data.get(login, -1)
+        if test == password:
+            return True
+        else:
+            return False
+
 if __name__ == '__main__':
     database = Database()
-    user = User(input("Введите логин:"), input("Введите пароль:"), input("Введите подтверждение пароля:"))
-    database.add_user(user.username, user.password)
+    while True:
+        choice = input("Приветствую! Выберите действие: \n0 - Вход\n1 - Регистрация\n")
+        if choice == '1':
+            user = User(input("Введите логин:"), password := input("Введите пароль:"), password2 := input("Введите подтверждение пароля:"))
+            if password != password2:
+                exit()
+            else:
+                database.add_user(user.username, user.password)
+
+        elif choice == '0':
+            login = input("Введите логин:")
+            password = input("Введите пароль:")
+            if database.is_autorize(login, password):
+                print("Авторизация успешна!")
+                exit()
+            else:
+                print("Не правильные имя пользователя и пароль!")
+                exit()
+
+
+
+
+
+
