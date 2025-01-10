2e99975c903f53a399e1efecd84ff230e3bc0c51
andrus777 <kotov.aa.logist@gmail.com>
2025-01-10 14:20:48 +0300

    module 7 task 5

--- module_7_5.py
+++ module_7_5.py
@@ -1,14 +1,21 @@
 import os
 import time
 
-directory = 'f:\\_sb'
+directory = 'd:\\Диалог'
+tab = "   "
 
 for root, dirs, files in os.walk(directory):
-    print("Directory path: %s" % root)
-    print("Directory Names: %s" % dirs)
-    print("Files Names: %s" % files)
+    for file in files:
+        filepath = os.path.join(root, file)
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
 
+        print(f'{tab}Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
 
-
-
-
+    for dir in dirs:
+        dirpath = os.path.join(root, file)
+        print(f'{tab}Обнаружена папка: {dir}, Путь: {os.path.join(root, dir)}, Размер: {os.path.getsize(os.path.join(root, dir))} байт, '
+              f'Время изменения: {time.strftime("%d.%m.%Y %H:%M", time.localtime(os.path.getmtime(dirpath)))}, Родительская директория: {os.path.dirname(dirpath)}')
+    tab = tab + "   "
